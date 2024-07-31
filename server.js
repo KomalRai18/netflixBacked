b438e7cfbfcb830ff0b5732394d64a5fd3ae8b45
Komal Rai <kr862913@gmail.com>
2024-07-31 17:01:05 +0500

    success

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 app.use(urlencoded({extended:true}));
 app.use(json());
 const corsOptions = {
-    origin: 'https://netflix-clone-sooty-pi.vercel.app',
+    origin: process.env.FRONTEND_URL,
     method: ["POST", "GET"],
     credentials: true,
 }
